e0d58060b261a171be816e09f749a7d6b459082c
Keller Jordan <kellerjordan1998@gmail.com>
2016-10-17 23:26:50 -0700

    added week 2, test week 0 files

--- week-2/client/main.js
+++ week-2/client/main.js
@@ -3,20 +3,50 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Images = new Mongo.Collection('images');
+
+Template.images.helpers({images:
+	Images.find({}, { sort: { createdOn: -1, rating: -1 } })
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.images.events({
+	'click .js-image'(event){
+		$(event.target).css('width', '50px');
+	},
+
+	'click .js-del-image'(event){
+		let image_id = this._id;
+		console.log(image_id);
+		$('#'+image_id).hide('slow', () => { Images.remove({ '_id': image_id }) });
+	},
+
+	'click .js-rate-image'(event){
+		let rating = $(event.currentTarget).data('userrating');
+		console.log(rating);
+		let image_id = this.id;
+		console.log(image_id);
+
+		Images.update({ _id: image_id }, { $set: { rating } });
+	},
+
+	'click .js-show-image-form'(event){
+		$('#image_add_form').modal('show');
+	}
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.image_add_form.events({
+	'submit .js-add-image'(event){
+		let img_src, img_alt;
+		img_src = event.target.img_src.value;
+		img_alt = event.target.img_alt.value;
+		console.log(`src: ${img_src} alt: ${img_alt}`);
+
+		Images.insert({
+			img_src: img_src, 
+			img_alt: img_alt, 
+			createdOn: new Date()
+		});
+		$('#image_add_form').modal('show');
+		return false;
+	}
 });
