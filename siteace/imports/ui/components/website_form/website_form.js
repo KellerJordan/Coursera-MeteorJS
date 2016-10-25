58eebb569082a1312898754073f30cb18f99cc08
KellerJordan <kellerjordan1998@gmail.com>
2016-10-25 03:11:22 -0700

    added http web scraping

--- siteace/imports/ui/components/website_form/website_form.js
+++ siteace/imports/ui/components/website_form/website_form.js
@@ -4,6 +4,10 @@ import { HTTP } from 'meteor/http';
 Template.website_form.helpers({
 	'recommended_sites_text'() {
 		return Session.get('showing_recommended') ? 'Show All Sites' : 'Show Only Recommended Sites';
+	},
+
+	'using_http'() {
+		return Session.get('using_http');
 	}
 })
 
@@ -14,17 +18,42 @@ Template.website_form.events({
 
 	'submit .js-save-website-form'(event) {
 		let form = event.target;
-		Websites.insert({
-			title: form.title.value,
-			url: form.url.value,
-			description: form.description.value,
-			time: new Date(),
-			votes: 0
-		});	
+		if(Session.get('using_http')) {
+			try {
+				HTTP.get(form.url.value, {}, (error, result) => {
+					if(error) {
+						console.log(error);
+					} else {
+						let title = $(result.content).filter('title').text();
+						Websites.insert({
+							title: title,
+							url: form.url.value,
+							description: form.description.value,
+							time: new Date(),
+							votes: 0
+						});
+					}
+				});			
+			} catch(err) {
+				console.log(err);
+			}
+		} else {
+			Websites.insert({
+				title: form.title.value,
+				url: form.url.value,
+				description: form.description.value,
+				time: new Date(),
+				votes: 0
+			});
+		}
 		return false;
 	},
 
 	'click .js-toggle-recommended-sites'(event) {
 		Session.set('showing_recommended', !Session.get('showing_recommended'));
+	},
+
+	'click .js-toggle-http'(event) {
+		Session.set('using_http', !Session.get('using_http'));
 	}
 });
